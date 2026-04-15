e9b90323ddf7a8271243e7a673a484664631be13
Diksha Nagpal <160147557+Diksha238@users.noreply.github.com>
2026-04-15 15:09:48 +0530

    Update allowed origins in CORS configuration

--- backend/src/main/java/com/dsareviewer/dsa_reviewer/config/SecurityConfig.java
+++ backend/src/main/java/com/dsareviewer/dsa_reviewer/config/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000", "https://dsa-reviewer-ui.vercel.app"));
+        config.setAllowedOrigins(List.of("http://localhost:3000", "https://dsa-reviewer-ui-9xtr.vercel.app"));
         config.setAllowedMethods(
                 List.of("GET","POST","PUT","DELETE","OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
